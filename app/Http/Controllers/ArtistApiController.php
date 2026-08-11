2d1a59ca3095be332328768bb42261f70eae7736
RENATO_ALVARES_DE_ALMEIDA <renatoalvares@OMEGAB52.BARROCA>
2026-08-11 09:40:29 -0300

    feat: create artist controller and register route

---
+++ app/Http/Controllers/ArtistApiController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Artist;
+use Illuminate\Http\Request;
+
+class ArtistApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $artists = Artist::all();
+        return $artists;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'profile_photo' => 'required|string',
+            'external_link' => 'string',
+            'song_sample' => 'binary',
+        ]);
+        $artist = Artist::create($validated);
+        return $artist;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $artist = Artist::findOrFail($id);
+        return $artist;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $artist = Artist::findOrFail($id);
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'profile_photo' => 'required|string',
+            'external_link' => 'string',
+            'song_sample' => 'binary',
+        ]);
+        $artist->update($validated);
+        return $artist;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $artist = Artist::findOrFail($id);
+        $artist->delete();
+        return response()->json(null, 204);
+    }
+}
