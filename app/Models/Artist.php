1e0d0a61a1f5ab18e85dc804f980d9afb57333ee
Renato Álvares <46536452+nato-re@users.noreply.github.com>
2026-08-13 11:32:05 -0300

    feat: Artist api (#7)
    * feat: cria model de artista
    
    * feat: criar rota resource /artists e controller
    
    ---------
    
    Co-authored-by: RENATO_ALVARES_DE_ALMEIDA <renatoalvares@DELTAF52.FLORESTA>

---
+++ app/Models/Artist.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    protected $fillable = [
+        "name",
+        "profile_picture",
+        "sample_song",
+    ];
+    protected $table = "artists";
+}
