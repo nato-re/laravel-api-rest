54a143da4e61512ca8f2f7a41e8b0a289a9d64a6
RENATO_ALVARES_DE_ALMEIDA <renatoalvares@DELTAF52.FLORESTA>
2026-08-13 16:44:52 -0300

    feat: add create artists migration

---
+++ database/migrations/2026_08_13_193955_create_artists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age')->nullable();
+            $table->string('profile_picture')->default('https://wallpapers.com/images/featured/cool-profile-pictures-87h46gcobjl5e4xu.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artists');
+    }
+};
